f2246f31e607998ee7a65d476f3ea0293d6cf08d
laihj <laihj@servyou.com.cn>
2018-07-30 17:03:26 +0800

    Sentinel的SPI引入，因为在ENV这个关键的类加载是触发，因而流程唯一程序流前面。

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/init/InitExecutor.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/init/InitExecutor.java
@@ -8,6 +8,9 @@ import com.alibaba.csp.sentinel.log.RecordLog;
 
 /**
  * Load registered init functions and execute in order.
+ * <p>
+ * 加载已注册的init函数并按顺序执行。
+ * </p>
  *
  * @author Eric Zhao
  */
@@ -18,17 +21,24 @@ public final class InitExecutor {
     /**
      * If one {@link InitFunc} throws an exception, the init process
      * will immediately be interrupted and the application will exit.
+     * <p>
+     * 如果一个InitFunc抛出异常，init进程将立即中断，应用程序将退出。
+     * </p>
      */
     public static void doInit() {
         if (initialized) {
             return;
         }
         try {
+            //加载InitFunc的实现
             ServiceLoader<InitFunc> loader = ServiceLoader.load(InitFunc.class);
+            //构建OrderWrapper链表节点由InitFunc实现以及他的优先级排序属性InitOrder完成。
+            //用户可以自定义加入
             List<OrderWrapper> initList = new ArrayList<OrderWrapper>();
             for (InitFunc initFunc : loader) {
                 insertSorted(initList, initFunc);
             }
+            //调用并初始化
             for (OrderWrapper w : initList) {
                 w.func.init();
             }
@@ -40,6 +50,12 @@ public final class InitExecutor {
         }
     }
 
+    /**
+     * 排序
+     *
+     * @param list
+     * @param func
+     */
     private static void insertSorted(List<OrderWrapper> list, InitFunc func) {
         int order = resolveOrder(func);
         int idx = 0;
@@ -51,6 +67,15 @@ public final class InitExecutor {
         list.add(idx, new OrderWrapper(order, func));
     }
 
+    /**
+     * 解析是否带有@InitOrder, 并读取其值
+     * <p>
+     * 有则是value，否则是LOWEST_PRECEDENCE
+     * </p>
+     *
+     * @param func
+     * @return
+     */
     private static int resolveOrder(InitFunc func) {
         if (!func.getClass().isAnnotationPresent(InitOrder.class)) {
             return InitOrder.LOWEST_PRECEDENCE;
@@ -59,8 +84,12 @@ public final class InitExecutor {
         }
     }
 
-    private InitExecutor() {}
+    private InitExecutor() {
+    }
 
+    /**
+     * 简单的进行包装
+     */
     private static class OrderWrapper {
         private final int order;
         private final InitFunc func;
--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/init/InitFunc.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/init/InitFunc.java
@@ -1,6 +1,8 @@
 package com.alibaba.csp.sentinel.init;
 
 /**
+ * spi接口进行提供初始化扩展点
+ *
  * @author Eric Zhao
  */
 public interface InitFunc {
