6647b90df69b27541c0e8272350d291df6219066
laihj <laihj@servyou.com.cn>
2018-07-31 19:41:49 +0800

    日志相关for 鹰眼

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/logger/LogSlot.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/logger/LogSlot.java
@@ -10,17 +10,21 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
 /**
  * A {@link com.alibaba.csp.sentinel.slotchain.ProcessorSlot} that is response for logging block exceptions
  * to provide concrete logs for troubleshooting.
+ * <p>
+ * {@link com.alibaba.csp.sentinel.slotchain.ProcessorSlot} 它是记录block exceptions，以提供用于故障排除的具体日志。
+ * </p>
  */
 public class LogSlot extends AbstractLinkedProcessorSlot<DefaultNode> {
 
     @Override
     public void entry(Context context, ResourceWrapper resourceWrapper, DefaultNode obj, int count, Object... args)
-        throws Throwable {
+            throws Throwable {
         try {
             fireEntry(context, resourceWrapper, obj, count, args);
         } catch (BlockException e) {
+            //记录日志，鹰眼追踪的日志记录
             EagleEyeLogUtil.log(resourceWrapper.getName(), e.getClass().getSimpleName(), e.getRuleLimitApp(),
-                context.getOrigin(), count);
+                    context.getOrigin(), count);
             throw e;
         } catch (Throwable e) {
             RecordLog.info("Entry exception", e);
